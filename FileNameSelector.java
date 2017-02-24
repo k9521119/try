c3dc88483310ccec4019de265af0e3e8b1da1c55
k9521119 <k9521119@outlook.com>
2017-02-24 22:48:35 +0800

    update FileNameSelector.java

--- FileNameSelector.java
+++ FileNameSelector.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.PrintWriter;
 
 /**
  * Created on 2017/2/24.
@@ -16,12 +17,12 @@ public class FileNameSelector implements FilenameFilter {
         return name.endsWith(extension);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         File directory = new File(".");
 //        System.out.println(directory);
 
         /**
-        // List al the files in directory
+        // List all the files in directory
         File[] files = directory.listFiles();
         System.out.println("\n目录" + directory.getName() + "下的所有文件:");
         for (File file : files) {
@@ -29,49 +30,19 @@ public class FileNameSelector implements FilenameFilter {
         }
          */
 
-        // List al the .txt files in directory
+        File txtFileList = new File("txtFileList.txt");
+        // Create a file to save the txt file list
+        PrintWriter output = new PrintWriter(txtFileList);
+
+        // List all the txt files in directory
         File[] txtFiles = directory.listFiles(new FileNameSelector("txt"));
-        System.out.println("\n当前目录下的所有txt文件:");
+//        System.out.println("\n当前目录下的所有txt文件:");
         for (File file : txtFiles) {
-            System.out.print(file.getName() + "\n");
+//            System.out.print(file.getName() + "\n");
+            output.write(file.getName() + "\r\n");
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        // Close file
+        output.close();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
